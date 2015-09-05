060da5742903fb0ab1ef81e29c1d68b3e93c5348
Hao Wang <hw1@cs.cmu.edu>
2015-09-04 23:46:22 -0400

    change hard coded klist in deblur_utils to be an input parameters so that global klist can be passed in

---
+++ scripts/yeast_analysis/read_align.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# pre-compiled STAR indices should be provided otherwise STAR won't run
+riboseq_fq=$1
+align_dir=$2
+if [ -z "${riboseq_fq}" ] || [ -z "${align_dir}" ]; then
+    echo "Usage: ./read_align.sh ribo-seq.fq.gz output_dir"
+    exit
+fi
+#=============================
+# default parameters
+#=============================
+cur_dir=`dirname $0`
+bin_dir=/home/hw1/scratch/software_testing/ribomap/bin/
+export PATH=${bin_dir}:$PATH
+mkdir -p ${align_dir}
+contaminant_idx=/home/hw1/scratch/scratch2/ribomap-playground/yeast/StarIndex/contaminant/
+transcript_idx=/home/hw1/scratch/scratch2/ribomap-playground/yeast/StarIndex/by_transcript/
+nproc=30
+adapter=N
+#============================================
+# step 1: filter rrna
+#============================================
+echo "filtering contaminated sequences in riboseq"
+ribo_core=`basename ${riboseq_fq}`
+ribo_core=${ribo_core%%.*}
+common_params="--runThreadN ${nproc} --clip3pAdapterSeq ${adapter} --seedSearchLmax 10 --outFilterMultimapScoreRange 0 --outFilterMultimapNmax 255 --outFilterMismatchNmax 1 --outFilterIntronMotifs RemoveNoncanonical"
+oriboprefix=${align_dir}${ribo_core}_rrna_
+ribo_nrrna_fa=${oriboprefix}Unmapped.out.mate1
+STAR --genomeDir ${contaminant_idx} --readFilesIn ${riboseq_fq} --outFileNamePrefix ${oriboprefix} --outStd SAM --outReadsUnmapped Fastx --outSAMmode NoQS "--readFilesCommand zcat <" ${common_params} > /dev/null 
+#============================================
+# step 2: map to transcriptome
+#============================================
+echo "aligning riboseq to the transcriptome"
+oriboprefix=${align_dir}${ribo_core}_transcript_
+STAR --genomeDir ${transcript_idx} --readFilesIn ${ribo_nrrna_fa} --outFileNamePrefix ${oriboprefix} --outSAMtype BAM Unsorted --outSAMmode NoQS --outSAMattributes NH NM ${common_params}
